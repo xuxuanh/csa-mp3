d1e1657d2a5745f709d5b8c38c48f0104f629f24
xuxuanh <xuxuanhe335@gmail.com>
2024-05-22 11:47:16 -0400

    Update Main.java

--- frq-solutions/q3/Main.java
+++ frq-solutions/q3/Main.java
@@ -1,30 +1,41 @@
-import java.util.*;
+import java.util.ArrayList;
 
-public class WordChecker {
-  private ArrayList<String> wordList;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("2024 FRQ Q3");
 
-  public WordChecker(ArrayList<String> words) {
-    wordList = words;
-  }
+    ArrayList<String> list1 = new ArrayList<String>();
+    list1.add("an");
+    list1.add("band");
+    list1.add("band");
+    list1.add("abandon");
+    ArrayList<String> list2 = new ArrayList<String>();
+    list2.add("to");
+    list2.add("too");
+    list2.add("stool");
+    list2.add("tools");
+    
+    WordChecker test1 = new WordChecker(list1);
+    WordChecker test2 = new WordChecker(list2);
 
-  public boolean isWordChain() {
-    for (int i = 1; i<wordList.size();i++){
-      if (wordList.get(i).indexOf(wordList.get(i-1))==-1){
-        return false;
-      }
-    }
-    return true;
-  }
+     //PART A TESTS
+     System.out.println(test1.isWordChain()); // expected result: true
+     System.out.println(test2.isWordChain()); // expected result: false
 
+    ArrayList<String> list3 = new ArrayList<String>();
+    list3.add("catch");
+    list3.add("bobcat");
+    list3.add("catchacat");
+    list3.add("cat");
+    list3.add("at");
 
-  public ArrayList<String> createList(String target) {
-    ArrayList<String> result = new ArrayList<String>();
-    for (int i = 0; i<wordList.size(); i++){
-      int index = wordList.get(i).indexOf(target);
-      if (index == 0){
-        result.add(wordList.get(i).substring(target.length()));
-      }
-    }
-    return result;             
+    WordChecker test3 = new WordChecker(list3);
+
+    // PART B TESTS
+    System.out.println(test3.createList("cat")); // ["ch", "chacat", ""]
+    System.out.println(test3.createList("catch")); // ["", "acat"]
+    System.out.println(test3.createList("dog")); // []
+    
+    
   }
 }
